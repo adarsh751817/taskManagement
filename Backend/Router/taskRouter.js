d650e6c44a5e63e7e2fa0d25725e2e1a707a23ea
adarsh751817 <145890098+adarsh751817@users.noreply.github.com>
2025-05-26 22:32:40 +0530

    Update taskRouter.js

--- Backend/Router/taskRouter.js
+++ Backend/Router/taskRouter.js
@@ -1,18 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const { createTask, deleteTask, updateTask, showTask } = require('../Controler/taskControler')
-
-
-
-// router.post('/create_Task', createTask);
-// router.delete('/delete_Task/:taskid', deleteTask);
-// router.put('/update_Task/:taskid', updateTask);
-// router.get('/show_Task', showTask);
-
-
-
-// module.exports = router; 
-
 
 const express = require('express');
 const router = express.Router();
