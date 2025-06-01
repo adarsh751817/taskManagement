71bdc2e4aaee1f6c48284e7983cbaa19285fbb47
adarsh751817 <145890098+adarsh751817@users.noreply.github.com>
2025-06-01 17:31:13 +0530

    Update taskModel.js

--- Backend/Model/taskModel.js
+++ Backend/Model/taskModel.js
@@ -1,5 +1,5 @@
-const   {mongoose, Schema }  = require('mongoose')
-// const mongoose = require('mongoose')
+const   {mongoose }  = require('mongoose')
+const { Schema } = mongoose;
 
 const taskSchema = new Schema({
     title: {
